0702a9aedd479cad19098b857e8b0153a2cabc5e
Pawel Szymanski <stasheq@gmail.com>
2020-08-15 18:53:59 +0200

    Started project

---
+++ app/src/main/java/me/szymanski/listtest/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.szymanski.listtest.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ logic/src/main/java/me/szymanski/logic/MyClass.kt
@@ -0,0 +1,4 @@
+package me.szymanski.logic
+
+class MyClass {
+}
