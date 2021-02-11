ae853bfd74bd8a3acc7b212e2666ff46537d13d2
Pawel Szymanski <stasheq@gmail.com>
2021-02-11 16:27:47 +0100

    Updated versions

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,5 +1,5 @@
 object Config {
     const val minSDK = 21
     const val targetSDK = 30
-    const val buildTools = "30.0.2"
+    const val buildTools = "30.0.3"
 }
--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -1,32 +1,35 @@
 object Deps {
     // https://kotlinlang.org/
-    const val kotlin = "1.4.20"
+    const val kotlin = "1.4.30"
 
     // https://github.com/Kotlin/kotlinx.coroutines
     const val coroutines = "1.4.2"
 
     // https://developer.android.com/jetpack/androidx/releases/lifecycle
-    const val androidLifecycle = "2.2.0"
+    const val androidLifecycle = "2.3.0"
 
     // https://developer.android.com/kotlin/ktx/extensions-list
     const val coreKtx = "1.3.2"
-    const val activityKtx = "1.1.0"
+    // https://developer.android.com/kotlin/ktx/extensions-list#androidxactivity
+    const val activityKtx = "1.2.0"
+    // https://developer.android.com/kotlin/ktx/extensions-list#androidxfragmentapp
     const val fragmentKtx = "1.2.5"
+    // https://developer.android.com/kotlin/ktx/extensions-list#androidxlifecycle
     const val viewModelKtx = "2.2.0"
 
     // https://github.com/google/dagger
-    const val dagger = "2.30.1"
+    const val dagger = "2.32"
     const val hilt = "$dagger-alpha"
 
     // https://developer.android.com/training/dependency-injection/hilt-jetpack
     // https://developer.android.com/jetpack/androidx/releases/hilt
-    const val hiltJetpack = "1.0.0-alpha02"
+    const val hiltJetpack = "1.0.0-alpha03"
 
     // https://github.com/square/retrofit/releases
     const val retrofit = "2.9.0"
 
     // https://square.github.io/okhttp/changelog/
-    const val okhttp = "4.9.0"
+    const val okhttp = "4.9.1"
 
     // https://developer.android.com/jetpack/androidx/releases/appcompat
     const val androidAppCompat = "1.2.0"
@@ -41,10 +44,10 @@ object Deps {
     const val recyclerView = "1.1.0"
 
     // https://github.com/material-components/material-components-android/releases
-    const val material = "1.2.1"
+    const val material = "1.3.0"
 
     // https://github.com/ReactiveX/RxJava/releases
-    const val rxJava = "3.0.7"
+    const val rxJava = "3.0.10"
 
     // https://github.com/JakeWharton/RxRelay/releases
     const val rxRelay = "3.0.0"
@@ -53,9 +56,9 @@ object Deps {
     const val rxAndroid = "3.0.0"
 
     // https://github.com/square/leakcanary/releases
-    const val leakCanary = "2.5"
+    const val leakCanary = "2.6"
 
     // tests
     // https://github.com/kotest/kotest/releases
-    const val kotest = "4.3.1"
+    const val kotest = "4.4.1"
 }
