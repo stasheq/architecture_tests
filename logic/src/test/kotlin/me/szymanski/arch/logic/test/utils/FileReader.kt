fca4dcf82d1fa8aae49c5e7b63c97dd6b9260e39
Pawel Szymanski <stasheq@gmail.com>
2021-11-22 11:17:47 +0100

    Update build.gradle.kts and FileReader.kt

--- logic/src/test/kotlin/me/szymanski/arch/logic/test/utils/FileReader.kt
+++ logic/src/test/kotlin/me/szymanski/arch/logic/test/utils/FileReader.kt
@@ -3,10 +3,9 @@ package me.szymanski.arch.logic.test.utils
 import java.io.BufferedReader
 
 object FileReader {
-    fun readText(fileName: String): String {
-        val reader = BufferedReader(javaClass.getResourceAsStream("/$fileName").reader())
-        val content: String
-        reader.use { content = it.readText() }
-        return content
-    }
+
+    fun readText(fileName: String): String =
+        BufferedReader(javaClass.getResourceAsStream("/$fileName")!!.reader()).use {
+            return@readText it.readText()
+        }
 }
