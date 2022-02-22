38f12cd7313f9e38cb33adb607143320e6b31b46
Pawel Szymanski <pawel@szymanski.me>
2022-02-22 08:06:51 +0100

    Update NavigationScreen.kt

--- logic/src/main/kotlin/me/szymanski/arch/logic/navigation/NavigationScreen.kt
+++ logic/src/main/kotlin/me/szymanski/arch/logic/navigation/NavigationScreen.kt
@@ -2,13 +2,13 @@ package me.szymanski.arch.logic.navigation
 
 import me.szymanski.arch.logic.details.RepositoryId
 import me.szymanski.arch.logic.navigation.StackBehavior.AddIfDifferent
-import me.szymanski.arch.logic.navigation.StackBehavior.Clear
+import me.szymanski.arch.logic.navigation.StackBehavior.Retrieve
 
 sealed interface NavigationScreen {
     val stackBehavior: StackBehavior
 
     data class List(
-        override val stackBehavior: StackBehavior = Clear
+        override val stackBehavior: StackBehavior = Retrieve
     ) : NavigationScreen
 
     data class Details(
@@ -18,10 +18,30 @@ sealed interface NavigationScreen {
 
     data class ListAndDetails(
         val repositoryId: RepositoryId?,
-        override val stackBehavior: StackBehavior = Clear
+        override val stackBehavior: StackBehavior = Retrieve
     ) : NavigationScreen
 }
 
 enum class StackBehavior {
-    Add, AddIfDifferent, Clear, Retrieve
+    /**
+     * Adds new screen to stack.
+     */
+    Add,
+
+    /**
+     * Adds new screen to stack, when current screen is of a different class.
+     * Updates args if current fragment is of the same class.
+     */
+    AddIfDifferent,
+
+    /**
+     * Clears stack to an empty state and then adds screen as first element.
+     */
+    Clear,
+
+    /**
+     * Searches for this screen class in a stack. If found, clears top of the stack
+     * and updates args in the found fragment. If not found, works same as Clear.
+     */
+    Retrieve
 }
